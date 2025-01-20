d1553c1d0a654ece4ce09d641a9c4602311a4801
Piotr Łepkowski <ozrabal@gmail.com>
2025-01-20 23:16:00 +0100

    feat: enhance Forgot Password page with new layout and reset password form, add message sanitization

--- src/app/(auth-pages)/forgot-password/page.tsx
+++ src/app/(auth-pages)/forgot-password/page.tsx
@@ -1,31 +1,36 @@
 import Link from "next/link";
 import { forgotPasswordAction } from "@/app/actions";
-import Button from "@/components/base/button";
+import PageWrapper from "@/components/base/pageWrapper";
+import { Message, sanitizeSearchParams, type MessageProps } from "@/components/modules/message";
+import { ResetPasswordForm } from "@/components/modules/resetPasswordForm";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-export default async function ForgotPassword() {
+export default async function ForgotPassword(props: { searchParams: Promise<MessageProps> }) {
+  const rawParams = await props.searchParams;
+  const sanitizedParams = sanitizeSearchParams(rawParams);
+
   return (
-    <>
-      <form>
-        <h1>Reset Password</h1>
-        <p>
-          Already have an account? <Link href="/sign-in">Sign in</Link>
-        </p>
-
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            required
-            name="email"
-            placeholder="you@example.com"
-          />
-          <Button
-            formAction={forgotPasswordAction}
-            type="submit"
-          >
-            Reset Password
-          </Button>
-        </div>
-      </form>
-    </>
+    <PageWrapper variant="centered">
+      {sanitizedParams ? (
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl">Reset Password</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-6">
+              <Message message={sanitizedParams} />
+              <Link
+                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                href="/sign-in"
+              >
+                <span>Back to Sign In</span>
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      ) : (
+        <ResetPasswordForm onSubmit={forgotPasswordAction} />
+      )}
+    </PageWrapper>
   );
 }
