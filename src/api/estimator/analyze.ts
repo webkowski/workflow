55d008c83bd5c02978c273d06f2272bbecd7b6aa
Piotr Łepkowski <ozrabal@gmail.com>
2025-02-22 01:00:30 +0100

    feat: retrieve task analyze prompt from database for text analysis

--- src/api/estimator/analyze.ts
+++ src/api/estimator/analyze.ts
@@ -1,15 +1,25 @@
 import { Hono } from "hono";
 import { streamText } from "ai";
 import { openai } from "@ai-sdk/openai";
-import { TASK_ESTIMATE } from "@/prompts";
+import { eq } from "drizzle-orm";
+import { db } from "@/db";
 
 const app = new Hono();
 app.post("/", async (c) => {
   const { prompt }: { prompt: string } = await c.req.json();
 
+  // @todo refactor to utils
+  const taskAnalyzePrompt = await db.query.prompts.findFirst({
+    where: (prompts) => eq(prompts.name, "TASK_ANALYZE"),
+  });
+
+  if (!taskAnalyzePrompt) {
+    return c.text("Task analyze prompt not found", 404);
+  }
+
   const result = streamText({
     model: openai("gpt-4o"),
-    system: TASK_ESTIMATE,
+    system: taskAnalyzePrompt?.prompt || "",
     prompt,
   });
 
