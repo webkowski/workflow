25b61b63e5ad316fecd6e99a5ff43b4babd92e22
Piotr Łepkowski <ozrabal@gmail.com>
2025-03-10 22:30:08 +0100

    refactor: simplify DropdownMenu and Input components by removing forwardRef and enhancing structure for better readability

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,21 +2,17 @@ import * as React from "react";
 
 import { cn } from "@/utils/ui";
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className,
-        )}
-        ref={ref}
-        type={type}
-        {...props}
-      />
-    );
-  },
-);
-Input.displayName = "Input";
+const Input = ({ className, type, ...props }: React.ComponentProps<"input">) => {
+  return (
+    <input
+      className={cn(
+        "border-input file:text-foreground placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        className,
+      )}
+      type={type}
+      {...props}
+    />
+  );
+};
 
 export { Input };
--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva, type VariantProps } from "class-variance-authority";
@@ -8,16 +6,14 @@ import { cn } from "@/utils/ui";
 
 const labelVariants = cva("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70");
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
+export type LabelProps = React.ComponentProps<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>;
+
+const Label = ({ className, ...props }: LabelProps) => (
   <LabelPrimitive.Root
     className={cn(labelVariants(), className)}
-    ref={ref}
+    data-slot="Label"
     {...props}
   />
-));
-Label.displayName = LabelPrimitive.Root.displayName;
+);
 
 export { Label };
