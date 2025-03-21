458e2593c5a12d275b66440b66a8ea27a896eb47
Piotr Łepkowski <ozrabal@gmail.com>
2025-03-21 08:13:10 +0100

    style: enhance layout styles for dashboard and header components for improved visual consistency

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import Chat from "@/components/modules/chat";
 
 export default function Page() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+    <div className="flex min-h-[1200px] flex-1 flex-col gap-4 p-6">
       <Chat />
     </div>
   );
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -42,7 +42,7 @@ export default async function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator
