9fc9592b6d03c8fee87cd52dc398e4eef913963f
Piotr Łepkowski <ozrabal@gmail.com>
2025-03-24 21:19:32 +0100

    feat: wrap LoginForm in Suspense for improved loading state handling

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { signInAction } from "@/app/actions";
 import PageWrapper from "@/components/base/pageWrapper";
 import { LoginForm } from "@/components/modules/loginForm";
@@ -5,7 +6,9 @@ import { LoginForm } from "@/components/modules/loginForm";
 export default async function Login() {
   return (
     <PageWrapper variant="centered">
-      <LoginForm onSubmit={signInAction} />
+      <Suspense fallback="Loading...">
+        <LoginForm onSubmit={signInAction} />
+      </Suspense>
     </PageWrapper>
   );
 }
